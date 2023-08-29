0ed45ca4991981d738fa5bc13fde224cb1ef3837
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-08-30 03:40:53 +0400

    update .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { MapComponents } from "./components/map/map";
+import { memo } from "react";
+
+import "./index.css";
+
+const MapComponentsMemo = memo(MapComponents);
+
 export const App = () => {
-  return <></>;
+  return (
+    <main>
+      <MapComponentsMemo />
+    </main>
+  );
 };
