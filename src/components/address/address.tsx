d48b42395d4de2cad0a133d74164f13c03725f5c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-09-03 06:26:10 +0400

    address add

---
+++ src/components/address/address.tsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { IStore } from "../../redux/type";
+
+export const Address = () => {
+  const userValue = useSelector<IStore, string>(
+    (store) => store.userValue.value
+  );
+  return (
+    <section className="w-1/3 h-40">
+      <article className="bg-slate-100 w-full h-full p-2 flex flex-col">
+        <span className="block font-bold text-slate-900 uppercase text-center">
+          where are we picking you from
+        </span>
+        <div className="flex flex-col align-items-center h-full justify-content-center">
+          <span className="block font-bold text-slate-500 uppercase text-center">
+            {userValue}
+          </span>
+        </div>
+      </article>
+    </section>
+  );
+};
