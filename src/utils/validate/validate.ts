6786d71782fd5408d8992d0c137e34dd264b531c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-09-04 02:27:53 +0400

    disabled eslint in validate

--- src/utils/validate/validate.ts
+++ src/utils/validate/validate.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { AnyAction, Dispatch } from "redux";
 import { Callback } from "../type";
 
@@ -6,7 +7,7 @@ export const ValidateInputUser = (
   dispatch: Dispatch<AnyAction>,
   callback: Callback
 ) => {
-  const regex = /^[а-яА-ЯёЁйЙ]+(\\,).+([0-9]+[а-яА-ЯёЁйЙ]+|[0-9]+)$/gm;
+  const regex = /^[а-яА-ЯёЁйЙ]+(\,).+([0-9]+[а-яА-ЯёЁйЙ]+|[0-9]+)$/gm;
   if (!regex.test(userValue.replaceAll(" ", ""))) {
     dispatch(callback({ error: "Enter a valid address" }));
     return false;
