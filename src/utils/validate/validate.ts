103557d9613b25602996fe1391a719841c8f3b39
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-09-04 02:25:02 +0400

    update validate regex

--- src/utils/validate/validate.ts
+++ src/utils/validate/validate.ts
@@ -6,7 +6,7 @@ export const ValidateInputUser = (
   dispatch: Dispatch<AnyAction>,
   callback: Callback
 ) => {
-  const regex = /^[а-яА-ЯёЁйЙ]+(\,).+([0-9]+[а-яА-ЯёЁйЙ]+|[0-9]+)$/gm;
+  const regex = /^[а-яА-ЯёЁйЙ]+(\\,).+([0-9]+[а-яА-ЯёЁйЙ]+|[0-9]+)$/gm;
   if (!regex.test(userValue.replaceAll(" ", ""))) {
     dispatch(callback({ error: "Enter a valid address" }));
     return false;
