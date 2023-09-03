8ef24e44c39040bc13ba956bfa69263374d58f36
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-09-04 00:30:41 +0400

    update logic cars create

--- src/utils/carsCreate/carsCreate.tsx
+++ src/utils/carsCreate/carsCreate.tsx
@@ -1,6 +1,7 @@
+import { SearchTaxiData } from "../../redux/taxiPlaceMarket/asyncAction/type";
 import { faker } from "@faker-js/faker";
 
-export const CarsCreate = () => {
+export const CarsCreate = (userPosition: SearchTaxiData) => {
   const cars = {
     code: faker.number.int({ min: 1, max: 99999 }),
     descr: "OK",
@@ -8,14 +9,18 @@ export const CarsCreate = () => {
       crews_info: [
         {
           crew_id: faker.number.int({ min: 1, max: 99999 }),
-          car_mark: faker.word.words(),
-          car_model: "",
+          car_mark: faker.vehicle.manufacturer(),
+          car_model: faker.vehicle.model(),
           car_color: faker.color.human(),
-          car_number: faker.vehicle.manufacturer(),
+          car_number: faker.vehicle.vrm(),
           driver_name: faker.person.firstName() + " " + faker.person.lastName(),
           driver_phone: faker.phone.number("+7-###-###-## ##"),
-          lat: 0,
-          lon: 0,
+          lat:
+            userPosition.addresses[0].lat +
+            faker.number.float({ min: -0.01, max: 0.01 }),
+          lon:
+            userPosition.addresses[0].lon +
+            faker.number.float({ min: -0.01, max: 0.01 }),
           distance: faker.number.int({ min: 1, max: 400 }),
         },
       ],
@@ -24,14 +29,18 @@ export const CarsCreate = () => {
   for (let i = 0; i <= faker.number.int({ min: 3, max: 10 }); i++) {
     cars.data.crews_info.push({
       crew_id: faker.number.int({ min: 1, max: 99999 }),
-      car_mark: faker.word.words(),
-      car_model: "",
+      car_mark: faker.vehicle.manufacturer(),
+      car_model: faker.vehicle.model(),
       car_color: faker.color.human(),
-      car_number: faker.vehicle.manufacturer(),
+      car_number: faker.vehicle.vrm(),
       driver_name: faker.person.firstName() + " " + faker.person.lastName(),
       driver_phone: faker.phone.number("+7-###-###-## ##"),
-      lat: 0,
-      lon: 0,
+      lat:
+        userPosition.addresses[0].lat +
+        faker.number.float({ min: -0.01, max: 0.01 }),
+      lon:
+        userPosition.addresses[0].lon +
+        faker.number.float({ min: -0.01, max: 0.01 }),
       distance: faker.number.int({ min: 1, max: 400 }),
     });
   }
