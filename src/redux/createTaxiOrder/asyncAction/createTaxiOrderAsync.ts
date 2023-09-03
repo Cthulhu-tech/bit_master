95ba15ea2d3acd4c44f8c69cf2b9cbb4ee3811de
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-09-04 01:42:25 +0400

    update createTaxiOrder

--- src/redux/createTaxiOrder/asyncAction/createTaxiOrderAsync.ts
+++ src/redux/createTaxiOrder/asyncAction/createTaxiOrderAsync.ts
@@ -1,11 +1,35 @@
+import { OrderDataTaxiInfo } from "../../type";
+import {
+  setOrderTaxiData,
+  setOrderTaxiDataLoading,
+} from "../../taxiOrderInfo/taxiOrderInfo";
 import { AppDispatch } from "../../root";
 import { CreateTaxiData } from "../type";
+import { faker } from "@faker-js/faker";
 
 export const FakeFetchDataTaxiCreateOrder =
   (userPosition: CreateTaxiData): any =>
   (dispatch: AppDispatch) => {
     console.log(userPosition);
+    dispatch(setOrderTaxiDataLoading({ loading: true }));
     return new Promise((resolve) => {
-      resolve("");
-    }).then((payload) => {});
+      setTimeout(
+        () =>
+          resolve({
+            code: faker.number.int({ min: 1, max: 99999 }),
+            descr: "Ok",
+            data: [
+              {
+                oreder_id: faker.number.int({ min: 1, max: 99999 }),
+              },
+            ],
+            loading: false,
+          }),
+        3000
+      );
+    }).then((payload) => {
+      console.log(payload);
+      dispatch(setOrderTaxiData(payload as OrderDataTaxiInfo));
+      dispatch(setOrderTaxiDataLoading({ loading: false }));
+    });
   };
